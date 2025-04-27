9a0aa35b32a43d0cfa5a9c4c7955a6ff6bc28767
selinzi <selinbekar9@gmail.com>
2025-04-27 18:15:31 +0300

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,11 @@
 function makeArray(firstArray, secondArray, maxLength) {
     const array = firstArray.concat(secondArray);
-    if (array.length >= maxLength) {
-        return array.slice(0,maxLength);
-    
-    } 
-        
-    
+    if (array.length > maxLength) {
+        return array.slice(0, maxLength);
+    } else {
+        return array;
+    }
+ 
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
